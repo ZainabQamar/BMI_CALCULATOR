77e5074bd7f7f927756fae6c4f52e8a4c676ad91
ZainabQamar <zainabbubak140@gmail.com>
2024-05-11 23:19:29 +0500

    bmi cl

---
+++ script.js
@@ -0,0 +1,5 @@
+const weight = document.querySelector(".weight");
+const unit = document.querySelector(".weight");
+const feet = document.querySelector(".feet");
+const inches = document.querySelector(".inches");
+console.log(weight);
