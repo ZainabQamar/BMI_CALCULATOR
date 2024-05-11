d7ca80d79919b73f2faf48fd2da6d566cd6d3dcd
ZainabQamar <zainabbubak140@gmail.com>
2024-05-12 01:27:44 +0500

    ...
    ...

--- script.js
+++ script.js
@@ -2,29 +2,39 @@ const weight = document.querySelector(".weight");
 const unit = document.querySelector(".unit");
 const feet = document.querySelector(".feet");
 const inches = document.querySelector(".inches");
+const result = document.querySelector(".result");
 console.log(weight);
 
 function calculateBMI() {
-  alert("calculateBM");
+  const height_in_meter = feet.value * 0.3048 + feet.value * 0.0254;
+  const BMI = weight.value / height_in_meter ** 2;
+  console.log(weight.value);
+  console.log(unit.value);
+  console.log(feet.value);
+  console.log(inches.value);
+  console.log(height_in_meter);
+  console.log(BMI);
+  result.textContent("BMI");
+  alert("calculateBMI");
 }
 
-function calculateBMI() {
-  let height = parseFloat(document.getElementById("height").value);
-  let weight = parseFloat(document.getElementById("weight").value);
+// function calculateBMI() {
+//   let height = parseFloat(document.getElementById("height").value);
+//   let weight = parseFloat(document.getElementById("weight").value);
 
-  let bmi = weight / Math.pow(height / 100, 2);
-  let bmiCategory;
+//   let bmi = weight / Math.pow(height / 100, 2);
+//   let bmiCategory;
 
-  if (bmi < 18.5) {
-    bmiCategory = "Underweight";
-  } else if (bmi < 24.9) {
-    bmiCategory = "Normal weight";
-  } else if (bmi < 29.9) {
-    bmiCategory = "Overweight";
-  } else {
-    bmiCategory = "Obesity";
-  }
+//   if (bmi < 18.5) {
+//     bmiCategory = "Underweight";
+//   } else if (bmi < 24.9) {
+//     bmiCategory = "Normal weight";
+//   } else if (bmi < 29.9) {
+//     bmiCategory = "Overweight";
+//   } else {
+//     bmiCategory = "Obesity";
+//   }
 
-  document.getElementById("result").innerText =
-    "Your BMI is: " + bmi.toFixed(2) + " (" + bmiCategory + ")";
-}
+//   document.getElementById("result").innerText =
+//     "Your BMI is: " + bmi.toFixed(2) + " (" + bmiCategory + ")";
+// }
