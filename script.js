c2c55375b2e0b010b09e292f816ea1221baa65f3
ZainabQamar <zainabbubak140@gmail.com>
2024-05-12 10:21:49 +0500

    ..

--- script.js
+++ script.js
@@ -3,38 +3,34 @@ const unit = document.querySelector(".unit");
 const feet = document.querySelector(".feet");
 const inches = document.querySelector(".inches");
 const result = document.querySelector(".result");
-console.log(weight);
 
 function calculateBMI() {
+  let convertedWeight = weight.value;
+  let status;
+  if (unit.value === "LB") {
+    convertedWeight = weight.value * 0.45359;
+  }
+
   const height_in_meter = feet.value * 0.3048 + feet.value * 0.0254;
-  const BMI = weight.value / height_in_meter ** 2;
-  console.log(weight.value);
+  const BMI = convertedWeight / height_in_meter ** 2;
+  console.log("convertedWeight", convertedWeight);
   console.log(unit.value);
   console.log(feet.value);
   console.log(inches.value);
-  console.log(height_in_meter);
-  console.log(BMI);
-  result.textContent("BMI");
-  alert("calculateBMI");
+  console.log(BMI.toFixed(2));
+
+  result.textContent = BMI.toFixed(2);
+
+  if (BMI.toFixed(2) < 18.5) {
+    status = "Underweight";
+  } else if (BMI.toFixed(2) < 24.9) {
+    status = "Normal weight";
+  } else if (BMI.toFixed(2) < 29.9) {
+    status = "Overweight";
+  } else {
+    status = "Obesity";
+  }
+
+  document.getElementsByClassName("result").innerText =
+    "Your BMI is: " + BMI.toFixed(2) + " (" + status + ")";
 }
-
-// function calculateBMI() {
-//   let height = parseFloat(document.getElementById("height").value);
-//   let weight = parseFloat(document.getElementById("weight").value);
-
-//   let bmi = weight / Math.pow(height / 100, 2);
-//   let bmiCategory;
-
-//   if (bmi < 18.5) {
-//     bmiCategory = "Underweight";
-//   } else if (bmi < 24.9) {
-//     bmiCategory = "Normal weight";
-//   } else if (bmi < 29.9) {
-//     bmiCategory = "Overweight";
-//   } else {
-//     bmiCategory = "Obesity";
-//   }
-
-//   document.getElementById("result").innerText =
-//     "Your BMI is: " + bmi.toFixed(2) + " (" + bmiCategory + ")";
-// }
